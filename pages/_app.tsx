fa339697ae6753e060b4304cdc1a8c8571994ff2
mcewball13 <mcewen1984@hotmail.com>
2022-10-06 16:49:34 -0700

    converted app to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import { ReactNode, ReactElement } from 'react'
+
+import { NextPage } from 'next';
+import App, { AppProps, AppContext } from 'next/app';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+interface MyAppProps extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, pageProps} = props;
   return <Component {...pageProps} />
 }
 
