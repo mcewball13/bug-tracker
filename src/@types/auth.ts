756c9f3eea92f9c4ca5cd46e0ee151c2dd2de232
mcewball13 <mcewen1984@hotmail.com>
2022-10-08 09:53:28 -0700

    added useEffect to check if accessToken exists and added login thunk

--- src/@types/auth.ts
+++ src/@types/auth.ts
@@ -1,5 +1,14 @@
-export type AuthState = {
-    isAuthenticated: boolean;
-    isInitialized: boolean;
-    user: Record<string, any> | null;
+export type ActionMap<M extends Record<string, any>> = {
+    [Key in keyof M]: M[Key] extends undefined
+        ? {
+              type: Key;
+          }
+        : {
+              type: Key;
+              payload: M[Key];
+          };
 };
+
+
+
+export type AuthUser = Record<string, any> | null;
--- src/contexts/JWTContext.tsx
+++ src/contexts/JWTContext.tsx
@@ -1,4 +1,7 @@
+import { stringify } from "querystring";
 import { createContext, ReactNode, useEffect, useReducer } from "react";
+import { ActionMap, AuthUser } from "src/@types/auth";
+import { isValidToken, setSession, decodeJWTUser } from "src/utils/jwt";
 
 import axios from "../utils/axios";
 
@@ -12,8 +15,6 @@ enum Actions {
     Register = "REGISTER",
 }
 
-type AuthUser = Record<string, any> | null;
-
 type JWTPayload = {
     [Actions.Initial]: {
         isAuthenticated: boolean;
@@ -26,14 +27,120 @@ type JWTPayload = {
     [Actions.Register]: {
         user: AuthUser;
     };
-}
+};
 
+type ActionTypes = ActionMap<JWTPayload>[keyof ActionMap<JWTPayload>];
 
+type AuthState = {
+    isAuthenticated: boolean;
+    isInitialized: boolean;
+    user: Record<string, any> | null;
+};
 
+const initialState: AuthState = {
+    isAuthenticated: false,
+    isInitialized: false,
+    user: null,
+};
 
+const JWTReducer = (state: AuthState, action: ActionTypes) => {
+    switch (action.type) {
+        case "INITIALIZE":
+            return {
+                isAuthenticated: action.payload.isAuthenticated,
+                isInitialized: true,
+                user: action.payload.user,
+            };
+        case "LOGIN":
+            return {
+                ...state,
+                isAuthenticated: true,
+                user: action.payload.user,
+            };
+        case "LOGOUT":
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null,
+            };
+        case "REGISTER":
+            return {
+                ...state,
+                isAuthenticated: true,
+                user: action.payload.user,
+            };
+        default:
+            return state;
+    }
+};
 
+type AuthProviderProps = {
+    children: ReactNode;
+};
 
-const JWTReducer = (state, action) => {
-    
+function AuthProvider({ children }: AuthProviderProps) {
+    const [state, dispatch] = useReducer(JWTReducer, initialState);
 
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const accessToken = localStorage.getItem("accessToken");
+
+                if (accessToken && isValidToken(accessToken)) {
+                    setSession(accessToken);
+
+                    const userId = decodeJWTUser(accessToken);
+                    // TODO - get user from backend
+                    const response = await axios.get(
+                        `api/account/my-account/${userId}`
+                    );
+
+                    const { user } = response.data;
+
+                    dispatch({
+                        type: Actions.Initial,
+                        payload: {
+                            isAuthenticated: true,
+                            user,
+                        },
+                    });
+                } else {
+                    dispatch({
+                        type: Actions.Initial,
+                        payload: {
+                            isAuthenticated: false,
+                            user: null,
+                        },
+                    });
+                }
+            } catch (err) {
+                console.error(err);
+                dispatch({
+                    type: Actions.Initial,
+                    payload: {
+                        isAuthenticated: false,
+                        user: null,
+                    },
+                });
+            }
+        };
+        init();
+    }, []);
+
+    const login = async (username: string, password: string) => {
+        const response = await axios.post("api/account/login", {
+            username,
+            password,
+        });
+        const { accessToken, user } = response.data;
+
+        setSession(accessToken);
+
+        dispatch({
+            type: Actions.Login,
+            payload: {
+                user,
+            },
+        });
+    };
 }
