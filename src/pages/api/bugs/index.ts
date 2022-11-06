7188998ac6f5ef23e18c6006b77930b905118452
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-11-05 18:06:51 -0700

    fixed bug index to follow new tech structure

--- src/pages/api/bugs/index.ts
+++ src/pages/api/bugs/index.ts
@@ -1,42 +1,53 @@
-import sequelize from '../../../../server/config/connection';
-import { Bug } from '../../../../server/models';
+import { useRouter } from 'next/router';
+import { NextApiRequest, NextApiResponse } from 'next';
+// models
+import { Bug } from '../../../../server/models/index.js';
+// Types
+import { RequestMethods as Methods } from '../../../@types/api';
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    
+    const { id } = req.query;
+    console.log(id);
+    const { method } = req
+    switch (method) {
+      case Methods.Get:
+        const bugs = await Bug.findAll({
+          attributes: {
+            exclude: ["id"],
+          },
+        });
+        res.status(200).json({ success: true, data: bugs });
+        break;
 
-export default handler;
+      // =================================================================================================
 
-function handler(req, res) {
-    switch (req.method) {
-        case 'GET':
-            return getBugs();
-        case 'POST':
-            return createBug(req.body);
-        default:
-            return res.status(405).end(`Method ${req.method} Not Allowed`)
-    };
+      case Methods.Post:
+        const newBug = await Bug.create({
+            title: req.body.title,
+            description: req.body.description,
+            priority: req.body.priority,
+            status: req.body.status,
+            dateCreated: new Date().toISOString(),
+            employee: req.body.employee_id
+        })
 
-
-    function getBugs() {
-        try {
-            const bugs = Bug.findAll();
-            return res.status(200).json(bugs);
-        } catch (error) {
-            return res.status(400).json({ message: error });
-        }
-    };
-
-    function createBug(req) {
-        try {
-            const newBug = Bug.create({
-                title: req.body.title,
-                description: req.body.description,
-                priority: req.body.priority,
-                status: req.body.status,
-                dateCreated: new Date().toISOString(),
-                employee: req.body.employee_id
-            })
-            return res.status(200).json({newBug});
-        } catch (error){
-            return res.status(400).json({ message: error});
-        }
-    };
+        res.status(200).json({ newBug });
+        break;
+      default:
+        res.setHeader('Allow', ['GET', 'POST']);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.errors,
+      type: error.errors,
+    });
+    console.log(error);
+  }
+  //   TODO add PUT and DELETE methods
 }
+
+
+
